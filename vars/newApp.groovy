c29f1a76ba87073d8915dcd3e45510ac6e396c2e
kingmoses33 <moses.kingsley33333@gmail.com>
2023-07-10 18:50:11 -0400

    pipeline amendment 3

--- vars/newApp.groovy
+++ vars/newApp.groovy
@@ -1,23 +1,23 @@
- def uber(String repoUrl){
-    pipeline{
-        agent any 
+def uber(String repoUrl){
+        pipeline{
+        agent any
         stages{
             stage('1-build'){
                 steps{
                     sh 'free -g'
                 }
             }
-            stage("Checkout Code") {
-               steps {
-                   git branch: 'main',
-                          url: "${repoUrl}"
+            stage('Checkout Code'){
+                steps{
+                    git branch: 'main',
+                    url: "${repoUrl}"
                 }
             }
             stage('closing'){
-             steps{
-                 echo "new demo"
-             }
+                steps{
+                    echo "new demo"
+                }
             }
+        }      
         }
     }
- }

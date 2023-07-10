b67df06ca8362972bb8ffbe5f02a509877d6cae3
kingmoses33 <moses.kingsley33333@gmail.com>
2023-07-10 18:36:35 -0400

    pipeline amendment 2

--- vars/newApp.groovy
+++ vars/newApp.groovy
@@ -7,10 +7,10 @@
                     sh 'free -g'
                 }
             }
-            stage('Checkout Code') {
-                steps {
-                    git branch: 'main',
-                           url: "${repoUrl}"
+            stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
                 }
             }
             stage('closing'){
